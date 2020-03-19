0417d21b85c69f4705f606a06aafbfffea1389f8
ThisIsIgrek <44672519+ThisIsIgrek@users.noreply.github.com>
2020-03-19 21:20:54 +0200

    Updates

---
+++ precache-manifest.d7a4269f8bca514d6fcedf05848fc736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea8daaa792c68fc18e8fa612f827462",
+    "url": "./index.html"
+  },
+  {
+    "revision": "66753bde06cd83dc21ff",
+    "url": "./static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "3b66e743b558ac3ec746",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "66753bde06cd83dc21ff",
+    "url": "./static/js/2.bf0441ef.chunk.js"
+  },
+  {
+    "revision": "3b66e743b558ac3ec746",
+    "url": "./static/js/main.3bb2a60f.chunk.js"
+  },
+  {
+    "revision": "9a27ebeb6340e57d0c9b",
+    "url": "./static/js/runtime-main.9853ae7f.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.159ff0f75295c98a502cfb564c63ff1e.js"
+  "./precache-manifest.d7a4269f8bca514d6fcedf05848fc736.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
